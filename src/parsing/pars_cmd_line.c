af49c9bc760269d2354b9f318ea9fec3e54bca6f
Zacharie LEMERY <zlemery@paul-f3Ar2s3.clusters.42paris.fr>
2023-06-15 17:13:28 +0200

    ignore separator

--- src/parsing/pars_cmd_line.c
+++ src/parsing/pars_cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: zlemery <zlemery@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:36:49 by zlemery           #+#    #+#             */
-/*   Updated: 2023/06/13 16:17:04 by zlemery          ###   ########.fr       */
+/*   Updated: 2023/06/15 16:35:03 by zlemery          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,23 @@
 
 int	is_quote(char *line, int i)
 {
-	int	 j;
+	int	j;
 	int	count;
 
 	j = 0;
 	count = 0;
-	while (j < i)
+	while (line[j] && j != i)
 	{
-		if (line[j] == '\'')
-			count++;
+		if (j > 0 && line[j - 1] == '\\')
+			;
+		else if (count == 0 && line[j] == '\"')
+			count = 1;
+		else if (count == 0 && line[j] == '\'')
+			count = 2;
+		else if (count == 1 && count == '\"')
+			count = 0;
+		else if (count == 2 && line[j] == '\'')
+			count = 0;
 		j++;
 	}
 	return (count);
@@ -31,7 +39,7 @@ int	is_quote(char *line, int i)
 
 int	is_sep(char *line, int i)
 {
-	if (ft_strchr("<>|;", line[i]) && is_quote(line, i) == 0)
+	if (i > 0 && ft_strchr("<>|;", line[i]) && is_quote(line, i) == 0)
 		return (1);
 	else
 		return (0);
@@ -68,7 +76,7 @@ char	*line_arg(char *line)
 	new = space_sep(line);
 	while (line[i])
 	{
-		if ((is_sep(line, i) == 1) && is_quote(line, i) != 2)
+		if ((is_sep(line, i) == 1) && is_quote(line, i) != 0)
 		{
 			new[j++] = ' ';
 			new[j++] = line[i++];
