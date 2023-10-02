e6d386caf7ff28d632495c9d7546dd21d57f9b55
Helene STEPHAN <hstephan@made-f0Dr8s1.clusters.42paris.fr>
2023-10-02 16:34:51 +0200

    echo en traitant les guillemets ouverts comme des caracteres lambdas

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,28 +6,48 @@
 /*   By: hstephan <hstephan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 11:15:54 by hstephan          #+#    #+#             */
-/*   Updated: 2023/10/02 15:22:45 by hstephan         ###   ########.fr       */
+/*   Updated: 2023/10/02 16:34:18 by hstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	print(char *str, int newline)
+static int	open_quote(char *str)
 {
-	int		i;
-	bool	open_quote;
+	int	i;
+	int	quotes;
 
 	i = 0;
-	open_quote = 0;
+	quotes = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '"')
+			quotes++;
+		i++;
+	}
+	return (quotes);
+}
+
+void	print(char *str, int newline, int i, int quote)
+{
+	int		n_quotes;
+
+	n_quotes = open_quote(str);
 	while (str && (str[i] == 32 || str[i] == '\t'))
 		i++;
 	while (str && str[i] != '\0')
 	{
-		if (str[i] == '"' && open_quote == 0)
-			open_quote = 1;
-		else if (str[i] == '"' && open_quote == 1)
-			open_quote = 0;
-		else if (open_quote == 1 || (str[i] != 32 && str[i] != '\t'))
+		if (str[i] == '"')
+		{
+			quote++;
+			if (quote == n_quotes && n_quotes % 2 == 1)
+				printf("%c", str[i]);
+			i++;
+		}
+		if ((quote != n_quotes && quote % 2 == 1)
+			|| (str[i] != 32 && str[i] != '\t'))
 			printf("%c", str[i]);
 		else if ((str[i] == 32 || str[i] == '\t')
 			&& str[i + 1] != 32 && str[i + 1] != '\t' && str[i + 1] != '\0')
@@ -66,26 +86,6 @@ static int	n_param(char *str, int *i, bool n, int last)
 	return (n);
 }
 
-// int	open_quote(char *str) / not in the mandatory new subjet
-// {
-// 	int	i;
-// 	int	quotes;
-
-// 	i = 0;
-// 	quotes = 0;
-// 	if (!str)
-// 		return (0);
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '"')
-// 			quotes++;
-// 		i++;
-// 	}
-// 	if (quotes % 2 != 0)
-// 		return (1);
-// 	return (0);
-// }
-
 // int	dquote(void) / not in the mandatory new subjet
 // {
 // 	char	*line;
@@ -108,9 +108,9 @@ int	ft_echo(char **tab)
 	if (tab && tab[1])
 	{
 		if (n_param(tab[1], &i, 0, 0) == 1)
-			print(&(tab[1][i]), 0);
+			print(&(tab[1][i]), 0, 0, 0);
 		else
-			print(&(tab[1][i]), 1);
+			print(&(tab[1][i]), 1, 0, 0);
 	}
 	else
 		printf("\n");
